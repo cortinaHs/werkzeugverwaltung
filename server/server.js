46930b224e5443fad0f0b23232348f3d2baf9fcd
Lisa Hilterhaus <lisa@hilterhaus.net>
2024-04-29 17:17:16 +0200

    add server and docker

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+//Create an app
+const app = express();
+app.get('/', (req, res) => {
+    res.send('Hello world\n');
+});
+
+//Listen port
+const PORT = 8080;
+app.listen(PORT);
+console.log(`Running on port ${PORT}`);
