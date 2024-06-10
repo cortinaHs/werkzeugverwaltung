642e16019fb4de91465f618e66ad6a998ba2ff62
Lisa Hilterhaus <lisa@hilterhaus.net>
2024-06-10 11:43:52 +0200

    new migration

--- src/app/Admin/page.jsx
+++ src/app/Admin/page.jsx
@@ -1,50 +1,66 @@
 import { auth } from "../auth";
+import { prisma } from "@/lib/prisma";
 import { redirect } from "next/navigation";
- 
-//TODO: DB connetion
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Separator } from "@/components/ui/separator";
+import { CategoryTable } from "./input";
+import { revalidatePath } from "next/cache";
 
-export  default async function Admin() {
-  const session = await auth();
-  
+export default async function Admin() {
+	const session = await auth();
+
 	if (!session) {
 		redirect("/signin");
 	}
 
-  if (session?.user.role != "admin") {
-      redirect("/search")
-}
-    return (
-      <div className="flex flex-col items-center min-h-screen p-4 bg-gray-100">
-        <div className="w-full max-w-3xl p-8 bg-white rounded-lg shadow-lg">
-          <h1 className="mb-6 text-3xl font-bold text-center">Administration</h1>
-          
-          <section className="mb-6">
-            <h2 className="mb-4 text-2xl font-bold">Liste aller Benutzer</h2>
-            <ul className="pl-5 text-gray-700 list-disc">
-              <li className="py-2 border-b border-gray-200">Benutzer 1</li>
-              <li className="py-2 border-b border-gray-200">Benutzer 2</li>
-              <li className="py-2 border-b border-gray-200">Benutzer 3</li>
-              {/* Fügen Sie hier die vollständige Liste der Benutzer hinzu */}
-            </ul>
-          </section>
+	if (session?.user.role != "admin") {
+		redirect("/search");
+  }
+  const categories = await prisma.category.findMany();
+  const users = await prisma.user.findMany();
   
-          <section className="mb-6">
-            <h2 className="mb-4 text-2xl font-bold">Werkzeugkategorien</h2>
-            <ul className="pl-5 mb-4 text-gray-700 list-disc">
-              <li className="py-2 border-b border-gray-200">Gartengeräte</li>
-              <li className="py-2 border-b border-gray-200">Gartenwerkzeug</li>
-              {/* Fügen Sie hier die vollständige Liste der Werkzeugkategorien hinzu */}
-            </ul>
-            <form className="flex space-x-2">
-              <input
-                type="text"
-                className="w-full p-2 text-gray-900 placeholder-gray-500 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
-                placeholder="Neue Kategorie hinzufügen"
-              />
-              <button type="submit" className="px-4 py-2 text-white bg-green-500 rounded hover:bg-green-600">Hinzufügen</button>
-            </form>
-          </section>
-        </div>
-      </div>
-    );
-  };
+  async function deleteCategory(id) {
+    "use server"
+    const deleteTools = prisma.tool.deleteMany({ where: { categoryId: id } });
+    const deleteCategory = prisma.category.delete({
+      where: { id },
+    });
+    const transaction = await prisma.$transaction([deleteTools, deleteCategory]);
+  }
+  async function addCategory(formData) { 
+    "use server"
+    const name = formData.get("name")
+    await prisma.category.create({
+      data: { name },
+    });
+  }
+  revalidatePath("/admin", "admin");
+  
+	return (
+		<main className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+			<div className="flex items-baseline justify-between pt-24 pb-6 border-b border-gray-200 ">
+				<h1 className="text-4xl font-bold tracking-tight text-gray-900">
+					Adminbereich
+				</h1>
+			</div>
+      <div className="flex flex-wrap py-6 mt-6 border-gray-100 -border-t md:flex-nowrap">
+        
+        <CategoryTable categories={categories}  deleteCategory={deleteCategory} addCategory={addCategory} />
+
+				<ScrollArea className="w-1/3 p-4 border rounded-md ">
+					<div className="p-4">
+						<h4 className="mb-4 text-sm font-medium leading-none">
+							Mitglieder
+						</h4>
+						{users.map((user) => (
+							<div key={user.id}>
+								<p>{user.name}</p>
+								<Separator className="my-2" />
+							</div>
+						))}
+					</div>
+				</ScrollArea>
+			</div>
+		</main>
+	);
+}
